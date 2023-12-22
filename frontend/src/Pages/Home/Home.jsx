9f9070509d5b680818c33d21d19a08ba747f7476
rohan0420 <skrohan0420@gmail.com>
2023-12-22 19:46:47 +0530

    created some components

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -7,38 +7,38 @@ function Home() {
 			<Navbar />
 			<Cart />
 			<main className="main">
-				<section class="home" id="home">
-					<div class="home__container container grid">
-						<div class="home__img-bg">
-							<img src={`${process.env.PUBLIC_URL}/assets/img/home.png`} alt="" class="home__img" />
+				<section className="home" id="home">
+					<div className="home__container container grid">
+						<div className="home__img-bg">
+							<img src={`${process.env.PUBLIC_URL}/assets/img/home.png`} alt="" className="home__img" />
 						</div>
 
-						<div class="home__social">
-							<a href="https://www.facebook.com/" target="_blank" class="home__social-link">
+						<div className="home__social">
+							<a href="https://www.facebook.com/" target="_blank" className="home__social-link">
 								Facebook
 							</a>
-							<a href="https://twitter.com/" target="_blank" class="home__social-link">
+							<a href="https://twitter.com/" target="_blank" className="home__social-link">
 								Twitter
 							</a>
-							<a href="https://www.instagram.com/" target="_blank" class="home__social-link">
+							<a href="https://www.instagram.com/" target="_blank" className="home__social-link">
 								Instagram
 							</a>
 						</div>
 
-						<div class="home__data">
-							<h1 class="home__title">NEW WATCH <br/> COLLECTIONS B720</h1>
-							<p class="home__description">
+						<div className="home__data">
+							<h1 className="home__title">NEW WATCH <br/> COLLECTIONS B720</h1>
+							<p className="home__description">
 								Latest arrival of the new imported watches of the B720 series,
 								with a modern and resistant design.
 							</p>
-							<span class="home__price">$1245</span>
+							<span className="home__price">$1245</span>
 
-							<div class="home__btns">
-								<a href="#" class="button button--gray button--small">
+							<div className="home__btns">
+								<a href="#" className="button button--gray button--small">
 									Discover
 								</a>
 
-								<button class="button home__button">ADD TO CART</button>
+								<button className="button home__button">ADD TO CART</button>
 							</div>
 						</div>
 					</div>
