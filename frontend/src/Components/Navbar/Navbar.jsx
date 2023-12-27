6a0342a1d758f357a887f0d0773ac85008854dda
rohan0420 <skrohan0420@gmail.com>
2023-12-27 20:24:34 +0530

    single product done nav done cart done mobile

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -102,8 +102,56 @@ function Navbar() {
                         </div>
 
                         <div className="cart__details">
-                            <h3 className="cart__title">Rose Gold</h3>
-                            <span className="cart__price">$850</span>
+                            <h3 className="cart__title">Jazzmaster</h3>
+                            <span className="cart__price">$1050</span>
+
+                            <div className="cart__amount">
+                                <div className="cart__amount-content">
+                                    <span className="cart__amount-box">
+                                        <i className='bx bx-minus' ></i>
+                                    </span>
+
+                                    <span className="cart__amount-number">1</span>
+
+                                    <span className="cart__amount-box">
+                                        <i className='bx bx-plus' ></i>
+                                    </span>
+                                </div>
+
+                                <i className='bx bx-trash-alt cart__amount-trash' ></i>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+
+                <hr />
+
+                <div className="cart__prices">
+                    <span className="cart__prices-item">3 items</span>
+                    <span className="cart__prices-total">$2880</span>
+                </div>
+                <div className='cart_checkoutBx'>
+                    <button className="button">
+                        Check Out <i className='bx bx-right-arrow-alt'></i>
+                    </button>
+                </div>
+            </div>
+
+            {/* user details sidebar*/}
+            <div className={`cart ${isActiveCart ? 'show-cart' : ''}`} id="cart">
+                <i className='bx bx-x cart__close' id="cart-close" onClick={handleClickCart}></i>
+
+                <h2 className="cart__title-center">Sk Rohan</h2>
+
+                <div className="cart__container">
+                    <article className="cart__card">
+                        <div className="cart__box">
+                            <img src={`${process.env.PUBLIC_URL}/assets/img/featured1.png`} alt="" className="cart__img" />
+                        </div>
+
+                        <div className="cart__details">
+                            <h3 className="cart__title">Jazzmaster</h3>
+                            <span className="cart__price">$1050</span>
 
                             <div className="cart__amount">
                                 <div className="cart__amount-content">
@@ -129,8 +177,8 @@ function Navbar() {
                         </div>
 
                         <div className="cart__details">
-                            <h3 className="cart__title">Longines Rose</h3>
-                            <span className="cart__price">$980</span>
+                            <h3 className="cart__title">Jazzmaster</h3>
+                            <span className="cart__price">$1050</span>
 
                             <div className="cart__amount">
                                 <div className="cart__amount-content">
@@ -151,9 +199,16 @@ function Navbar() {
                     </article>
                 </div>
 
+                <hr />
+
                 <div className="cart__prices">
                     <span className="cart__prices-item">3 items</span>
                     <span className="cart__prices-total">$2880</span>
+                </div>
+                <div className='cart_checkoutBx'>
+                    <button className="button">
+                        Check Out <i className='bx bx-right-arrow-alt'></i>
+                    </button>
                 </div>
             </div>
         </>
