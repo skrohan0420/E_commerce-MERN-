4fdd61a4d7e9851073fb5a8c72d91e6797807d6e
rohan0420 <skrohan0420@gmail.com>
2024-01-09 17:22:05 +0530

    backend started

---
+++ backend/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const dbURI = 'mongodb+srv://BecGbWLy2urBa0gE:BecGbWLy2urBa0gE@rohan.baf7f9w.mongodb.net/e_commerce?retryWrites=true&w=majority'
+
+const mongoDB = () => {
+
+    mongoose.connect(dbURI)
+    .then(result => {
+        if(result){
+            console.log('---- DB Connected')
+        }else{
+            console.log('---- DB Not Connected')
+        }
+    })
+    .catch(err => console.log(err));
+}
+
+module.exports = mongoDB;
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 5000
-
+const mongoDB = require('./db')
+mongoDB();
 
 
 
